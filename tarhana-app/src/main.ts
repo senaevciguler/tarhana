8d8f5493c0adee969a13b6649b3a92b5f379a195
mehmetalp <mehmet.guler@maginteractive.se>
2026-02-14 21:55:46 +0100

    initial commit

--- tarhana-app/src/main.ts
+++ tarhana-app/src/main.ts
@@ -3,7 +3,5 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app';
 import 'zone.js';
 
-
-
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
