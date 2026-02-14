a5f9f484f89c7d888c442a2a78fa6d07c1abfd5f
mehmetalp <mehmet.guler@maginteractive.se>
2026-02-14 22:00:53 +0100

    initial commit

--- tarhana-app/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { RecipesComponent } from './pages/recipes/recipes';
-import { ShopComponent } from './pages/shop/shop';
-
-export const routes: Routes = [
-    { path: '', redirectTo: 'recipes', pathMatch: 'full' },
-    { path: 'recipes', component: RecipesComponent },
-    { path: 'shop', component: ShopComponent },
-];
--- tarhana-app/src/app/components/footer/footer.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-footer',
-  standalone: true,
-  imports: [], // Removed RouterLink as it was unused and caused a warning
-  templateUrl: './footer.component.html',
-})
-export class FooterComponent {}
